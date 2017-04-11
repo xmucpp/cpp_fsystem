09ce3b589cb5a3e110feb732024b6716a3491da7
sinnfashen <tanghanlincpp@163.com>
2017-04-11 21:15:43 +0800

    new 0.8

--- Subfunc/Crawler/JDPageScraper.py
+++ Subfunc/Crawler/JDPageScraper.py
@@ -1,255 +1,161 @@
 # __author__ = 'xjlin'
 # -*- coding: utf-8 -*-
 
-import os
-import codecs
-import csv
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+import requests
 import time
 import random
-import re
-import requests
 import json
-
-from bs4 import BeautifulSoup
-
-from Subfunc.Crawler import USER_AGENTS
+import csv
+import re
+import os
 from Work.log import Logger
+import gevent
+from gevent import monkey;monkey.patch_all()
 logger = Logger('JD', 'DEBUG')
 
-totalurl = ''
-# Unit Function
 
+class Jd_scraper(object):
+    def __init__(self,url,sort):
+        PATH = os.path.join(os.getcwd())
+        PRESENT_TIME = str(time.strftime('%H-%M-%S', time.localtime(time.time())))
+        PRESENT_DAY = str(time.strftime('%Y-%m-%d', time.localtime(time.time())))
+        #1为销量排序，0为综合排序
+        self.sort_dict ={0:'',1:'1'}
+        self.sort = sort
 
-def get_html(url):
-    """
-    get html content
-    Parameters
-    ----------
-    url
+        self.url = url
+        self.search_list_url = 'https://so.m.jd.com/ware/searchList.action'
 
-    Returns
-    -------
+        self.categoryId = re.findall(r'-(\d+).html',url)[0]
+        self.c1 = url.split('-')[0].split('/')[-1]
+        self.c2 = url.split('-')[-2]
 
-    """
-    user_agent = random.choice(USER_AGENTS)
-    headers = {
-        'Accept': 'text/html',
-        'Accept-Encoding': 'gzip,deflate,sdch',
-        'Accept-Language': 'zh-CN,zh;q=0.8',
-        'Cache-Control': 'max-age=0',
-        'Connection': 'keep-alive',
-        'user-agent': user_agent,
-    }
-    if url.find('p.3.cn'):
-        headers['Host'] = 'p.3.cn'
-        headers['Referer'] = totalurl
-    else:
-        headers['Host'] = 'list.jd.com'
-    try:
-        r = requests.get(url, headers=headers)
-        if r.status_code == 302:
-            logger.error('this server be banned by JD')
-            time.sleep(30)
-            raise
-        else:
-            return r.content
-    except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout, requests.exceptions.MissingSchema) as e:
-        logger.error('{} {}'.format(url.encode('utf-8'), 'Connect Error'))
-        raise e
-
-
-def parse_html(html, url, page_num, cate, sort):
-    PRESENT_TIME = str(time.strftime('%H-%M-%S', time.localtime(time.time())))
-    PRESENT_DAY = str(time.strftime('%Y-%m-%d', time.localtime(time.time())))
-    if not html:
-        logger.error('not get html')
-        return None
-    data_list = []
-    try:
-        soup = BeautifulSoup(html, 'html.parser')
-        soup = soup.find('div', 'g-main2')
-        IDs = []
-        IDprice = ''
-        IDcomment = []
-        div_gls = []
-        for i in soup.find_all('div', 'tab-content-item tab-cnt-i-selected j-sku-item'):
-            div_gls.append(i.find('div', 'p-name'))
-        for i in soup.find_all('div', 'gl-i-wrap j-sku-item'):
-            div_gls.append(i.find('div', 'p-name'))
-        for i in range(0, div_gls.__len__()):
-            div = div_gls[i]
-            data = {}
-            try:
-                data['goodsName'] = div.em.text.encode('utf-8')
-            except Exception, e:
-                logger.warning('{} {} {}'.format(url, e, 'goods name is None'))
-                data['goodsName'] = None
-
-            try:
-                data['ID'] = re.search(r'\d+', div.a['href']).group()
-                IDs.append(data['ID'])
-            except Exception, e:
-                logger.warning('{} {} {}'.format(url, e, 'id is None'))
-                data['ID'] = None
-            data_list.append(data)
-        for ID in IDs:
-            IDprice = '{}J_{}%2C'.format(IDprice, str(int(ID.replace(':[]', ''))))
-            IDcomment.append(int(ID.replace(':[]', '')))
-        try:
-            prices = json.loads(get_html('{}{}'.format('http://p.3.cn/prices/mgets?http://p.3.cn/prices/mgets?type=1&area=1_72_4137_0&pdbp=0&pdtk=&pdpin=&pduid=14875031659431915603908&skuIds=', IDprice)))
-            jscomments = get_html('{}{}'.format('http://club.jd.com/comment/productCommentSummaries.'
-                                                'action?referenceIds=',
-                                                str(IDcomment)[1:-1].replace(' ', '').replace('L', '')))
-            comments = json.loads(jscomments.decode('gbk').encode('utf8'))['CommentsCount']
-        except Exception, e:
-            logger.error('{} {}'.format(url, 'js error'))
-        for i in range(0, div_gls.__len__()):
-            try:
-                if prices[i]['id'][2:] == data_list[i]['ID']:
-                    data_list[i]['price'] = prices[i]['p']
-                else:
-                    raise KeyError
-            except Exception, e:
-                logger.warning('{} {} {}'.format(url, 'price is None', e))
-                data_list[i]['price'] = None
-
-
-            try:
-                if comments[i]['SkuId'] == int(data_list[i]['ID']):
-                    data_list[i]['commentsNum'] = comments[i]['CommentCount']
-                    data_list[i]['goodRate'] = comments[i]['GoodRate']
-                    data_list[i]['generalRate'] = comments[i]['GeneralRate']
-                    data_list[i]['poorRate'] = comments[i]['PoorRate']
-                    data_list[i]['averageScore'] = comments[i]['AverageScore']
-                    data_list[i]['date'] = PRESENT_DAY
-                    data_list[i]['time'] = PRESENT_TIME
-                    data_list[i]['page'] = page_num
-                    data_list[i]['cate'] = cate
-                    data_list[i]['sort'] = sort
-                else:
-                    raise KeyError
-            except Exception, e:
-                logger.warning('{} {} {}'.format(url, e, 'comments num is None'))
-                data_list[i]['commentsNum'] = data_list[i]['goodRate'] = data_list[i]['generalRate'] = data_list[i]['poorRate'] = data_list[i]['averangeScore'] = None
-    except AttributeError, e:
-        logger.error('{} {} {}'.format(url, 'parse error', e))
-        return None
-    return data_list
-
-
-def write_csv(data_list, url, paras):
-    """
-    Write the data list into a .csv file
-
-     Parameters
-     ----------
-      data_list: list
-         elements with type "dict"
-
-     Returns
-     -------
-      indicator: bool
-          success with "True" and failure with "None"
-    """
-    PATH = os.path.join(os.getcwd())
-    PRESENT_TIME = str(time.strftime('%H-%M-%S', time.localtime(time.time())))
-    PRESENT_DAY = str(time.strftime('%Y-%m-%d', time.localtime(time.time())))
-    if not data_list:
-        logger.error('{} {}'.format(url, 'data list is None'))
-        return None
-
-    # check or create a daily dictionary
-    try:
+        #self.page_num =self.get_page_num()
+        #文件名为url里的参数和爬取时间
+        self.date = PRESENT_DAY
+        self.time = PRESENT_TIME
         file_dict = os.path.join(PATH, 'Data', 'JDData', '{}{}'.format('JDData_', PRESENT_DAY))
-    except TypeError:
-        file_dict = ''.join(['../JDData_', PRESENT_DAY])
-    try:
-        os.makedirs(file_dict)
-    except OSError, e:
-        if e.errno != 17:
-            raise e
+        self.save_name = ''.join([
+            file_dict, '/', 'jd_', self.date, '_', self.time, '_', self.c1, '_', self.c2, '_', self.categoryId, '_', str(self.sort)])
 
-    try:
-        page_num = data_list[0]['page']
-        sort = data_list[0]['sort']
-    except Exception:
-        raise
+    def get_comment_info(self,id):
+        url ='http://club.jd.com/comment/productCommentSummaries.action?'
+        headers = {'User-Agent':'Mozilla/5.0 (Linux; U; Android 2.3; en-us) AppleWebKit/999+ (KHTML, like Gecko) Safari/999.9',
+                   'Host':'club.jd.com',
+                   'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+        }
+        payload = {'referenceIds':id}
+        try:
+            r = requests.get(url,headers =headers,params = payload)
+            json_info = json.loads(r.text)['CommentsCount'][0]
+            return json_info
+        except:
+            return None
 
-    # create a file and its name for a certain page
-    file_name = ''.join(
-        [file_dict, '/', 'jdPrice2', '_', PRESENT_DAY, '_', PRESENT_TIME, '_',
-         paras, '_', str(sort), '_', str(page_num)])
-
-    with codecs.open(file_name, 'wb') as f:
-        fieldnames = ['goodsName', 'ID', 'price', 'commentsNum', 'averageScore', 'goodRate', 'generalRate', 'poorRate',
-                      'date', 'time', 'page', 'cate', 'sort']
-        writer = csv.DictWriter(f, fieldnames=fieldnames)
-        writer.writeheader()
-        for data in data_list:
-            data = {key: value for key, value in data.items() if key in fieldnames}
-            writer.writerow(data)
-
-    return [itemid['ID'] for itemid in data_list]
-
-
-def split_url(url):
-
-    regx_page = re.compile('page=\d+')
-
-    page_num = regx_page.search(url).group().split('=')[1]
-    page_num = int(page_num)
-
-    regx_cat = re.compile('cat=\d+,\d+,\d+')
-    cat = regx_cat.search(url).group().split('=')[1]
-
-    regx_catnum = re.compile('\d+')
-    catall = re.findall(regx_catnum, cat)
-    cat1 = catall[0]
-    cat2 = catall[1]
-    cat3 = catall[2]
-
-    regx_sort = re.compile('&sort')
-    sort = regx_sort.search(url)
-    # 1为销量排序, 0为综合排序, 为文件名的倒数第二个参数, 在csv文件中为sort的值
-    if sort:
-        sort = 1
-    else:
-        sort = 0
-
-    return page_num, cat1, cat2, cat3, sort
-
-
-# Integration Function
-
-def structure(category_name, page_num=2):
-    for i in xrange(1, page_num + 1):
-        yield 'http://{}&page={}'.format(category_name, i)
-        yield 'http://{}&page={}&sort=sort_totalsales15_desc'.format(category_name, i)
-
-
-def parse(url):
-    global totalurl
-    totalurl = url
-    logger.info(url)
-    try:
-        page_num, cat1, cat2, cat3, sort = split_url(url)
-        paras = '{},{},{}'.format(cat1, cat2, cat3)
-        html = get_html(url)
-        data_list = parse_html(html, url, page_num, cat3, sort)
-        if data_list:
-            write_csv(data_list, url, paras)
-            return 0
+    def get_html(self,page_num=1):
+        #sort1表示按销量顺序
+        data={'_format_':'json',
+              'categoryId': self.categoryId,
+              'c1': self.c1,
+              'c2': self.c2,
+              'sort': self.sort_dict[self.sort],
+              'page':str(page_num)}
+        headers = {'authority':'so.m.jd.com',
+        'method':'POST',
+        'path':'/ware/searchList.action',
+        'scheme':'https',
+        'accept':'application/json',
+        'referer':self.url,
+        'user-agent':'Mozilla/5.0 (Linux; U; Android 2.3; en-us) AppleWebKit/999+ (KHTML, like Gecko) Safari/999.9',
+        'x-requested-with':'XMLHttpRequest'}
+        time.sleep(random.randint(1,3))
+        r = requests.post(self.search_list_url,headers=headers,data=data)
+        if r.status_code == 200:
+            print('Success to get html')
+            return r.text
         else:
-            return 0
+            print('Fail to get html')
+            return None
+
+    #获取总面数
+    def get_page_num(self):
+        html = self.get_html()
+        try:
+            pagenum = int(json.loads((json.loads(html)['value']))['wareList']['wareCount'])//10
+            print('Success to get page_num:',pagenum)
+            return pagenum
+
+        except KeyError:
+            print('Fail to get page_num')
+            return None
+
+    def parse(self,pagenum):
+        r = self.get_html(page_num=pagenum)
+        ware_list = json.loads((json.loads(r)['value']))['wareList']['wareList']
+        wholeinfo =[]
+        for index,i in enumerate(ware_list):
+            info=[]
+            #表示在第几页第几个 如1-2表示第一页第二个
+
+            info.append(i['wname'].encode('utf8'))
+            info.append(i['wareId'].encode('utf8'))
+            info.append(i['jdPrice'].encode('utf8'))
+            info.append(i['totalCount'].encode('utf8'))
+            comment_info = self.get_comment_info(i['wareId'].encode('utf8'))
+            info.append(comment_info['AverageScore'])
+            info.append(comment_info['GoodRate'])
+            info.append(comment_info['GeneralRate'])
+            info.append(comment_info['PoorRate'])
+            info.append(self.date)
+            info.append(self.time)
+            info.append(str(pagenum) + '-' + str(index + 1))
+            info.append(self.categoryId)
+            info.append(self.sort)
+            #print(info)
+
+            wholeinfo.append(info)
+        self.save_csv(wholeinfo)
+
+    def create_csv(self):
+        titles=['goodsName','ID','price','commentsNum','averageScore','goodrate','generalRate','poorRate','date','time','page','cate','sort']
+        with open(self.save_name, 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(titles)
+
+    def save_csv(self,infolist):
+        with open(self.save_name, 'ab') as csvfile:
+            writer = csv.writer(csvfile)
+            print(infolist)
+            print(type(infolist))
+            writer.writerows(infolist)
+
+    def run(self):
+        self.create_csv()
+        jobs=[]
+        for i in range(10):
+            jobs.append(gevent.spawn(self.parse,i+1))
+        gevent.joinall(jobs)
+
+
+def main(url):
+    #综合排序
+    try:
+        c =Jd_scraper(url,0)
+        c.run()
+
+    #销量排序
+        m =Jd_scraper(url,1)
+        m.run()
     except Exception:
         logger.error(logger.traceback())
-        return 0
 
+if __name__ == "__main__":
+    url = 'https://so.m.jd.com/products/1318-12099-9755.html'
+    main(url)
 
-if __name__ == '__main__':
-    url = 'http://list.jd.com/list.html?cat=9987,653,655&page=2&sort=sort_totalsales15_desc'
-    parse(url)
 
 crawler = {
-    'JD': parse
+    'JD': main
 }
