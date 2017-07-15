d3b6f2a98d2470e00ea890e490db6225da940b96
sinnfashen <tanghanlincpp@163.com>
2017-07-15 19:16:09 +0800

    update

--- Function/crawler.py
+++ Function/crawler.py
@@ -46,7 +46,7 @@ def ticker(worker_name):
     thread = threading.Thread(target=middle, args=[worker_name, event])
     thread.setDaemon(True)
     thread.start()
-    event.wait(120)
+    event.wait(180)
     if event.isSet():
         return
     else:
